b507a9cbbf716cd87e0907c79eb85452cc502649
Ganesh Kumar <GaneshKumar.Kathirvel@amd.com>
2020-06-18 03:05:30 +0000

    ROCm stage1 spack recipes

---
+++ packages/roctracer-dev/package.py
@@ -0,0 +1,34 @@
+from spack import *
+
+class RoctracerDev(CMakePackage):
+    """ROC-tracer library: Runtimes Generic Callback/Activity APIs.
+       The goal of the implementation is to provide a generic independent from
+       specific runtime profiler to trace API and asyncronous activity."""
+
+    homepage = "https://github.com/ROCm-Developer-Tools/roctracer"
+    url      = "https://github.com/ROCm-Developer-Tools/roctracer/archive/rocm-3.5.0.tar.gz"
+
+    # maintainers = ['github_user1']
+
+    version('3.5.0', sha256='7af5326c9ca695642b4265232ec12864a61fd6b6056aa7c4ecd9e19c817f209e')
+
+    depends_on('cmake@3.5.2', type='build')
+    depends_on('hsakmt-roct@3.5.0:', type='build', when='@3.5.0:')
+    depends_on('hsa-rocr-dev@3.5.0:', type='build', when='@3.5.0:')
+    depends_on('rocminfo@3.5.0:', type='build', when='@3.5.0:')
+    depends_on('hip@3.5.0:', type='build', when='@3.5.0:')
+
+    def setup_build_environment(self, build_env):
+        spec=self.spec
+        build_env.set("HIP_PATH", spec['hip'].prefix)
+
+    def patch(self):
+        filter_file('${CMAKE_PREFIX_PATH}/hsa', '${HSA_RUNTIME_INC_PATH}', 'src/CMakeLists.txt', string=True)
+
+    def cmake_args(self):
+        args = [
+                '-DHIP_VDI=1',
+                '-DCMAKE_MODULE_PATH={}/cmake_modules'.format(self.stage.source_path),
+                '-DHSA_RUNTIME_HSA_INC_PATH={}/include'.format(self.spec['hsa-rocr-dev'].prefix)
+               ]
+        return args
